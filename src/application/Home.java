ed03af900c1345f912775c8531c5bae393d45d14
Arvin Ferrer <60701881+arvin-ferrer@users.noreply.github.com>
2025-12-01 14:37:42 +0800

    feat: add different scene for home

---
+++ src/application/Home.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import Soldiers.Archer;
+import Soldiers.Projectile;
+import Soldiers.Soldier;
+import Soldiers.Spearman;
+import Zombies.NormalZombie;
+import Zombies.TankZombie;
+import Zombies.Zombie;
+import Zombies.nurseZombie;
+import javafx.animation.AnimationTimer;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+public class Home {
+
+}
